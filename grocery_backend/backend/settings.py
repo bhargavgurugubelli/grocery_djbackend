881a41719a0476e992ad6b7fb5aec5372ff995ec
bhargavgurugubelli <gurugubellibhargav@gmail.com>
2025-04-09 15:04:34 +0530

    Initial commit: added env and updated settings

--- grocery_backend/backend/settings.py
+++ grocery_backend/backend/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+from decouple import config
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o4x-(#j1rx@=r7z32sk20sk0^e0mhho#)auch+tg*y1vct9-hw"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
